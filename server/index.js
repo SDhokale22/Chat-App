7bbcc51f6bbed46c015d33f741e91b695ecc8de8
Sonali Dhokale <sonadhokale22@gmail.com>
2024-05-05 20:39:35 +0530

    Updated code

--- server/index.js
+++ server/index.js
@@ -6,15 +6,22 @@ import messageRoute from "./routes/messageRoutes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./socket/socket.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config({});
 
 //const app = express();
 const PORT = process.env.PORT || 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, './client/build')));
 
 const corsOption={
     origin:"http://localhost:3000",
@@ -24,6 +31,15 @@ app.use(cors(corsOption));
 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/message", messageRoute);
+
+app.use('*', function(req, res) {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+  
+
+app.get("/", (req,res) => {
+   res.send("<h1>Welcome to Real-time Chat app</h1>");
+});
 
 server.listen(PORT, () => {
     connectDB();
